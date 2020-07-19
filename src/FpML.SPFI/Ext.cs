f54180d9cca0fcedd3bd62c798ac4de9cce18907
Ilya Boyarskiy <lppt@lppt-mbpro.local>
2020-07-19 12:34:14 +0300

    добавил NSD и SPFI

---
+++ src/FpML.SPFI/Ext.cs
@@ -0,0 +1,28 @@
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace SoftWell.Ctor.Api.Contract.FpmlSpfi
+{
+    public partial class FxSingleLeg
+    {
+        private bool tenorNameFieldSpecified;
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool tenorNameSpecified
+        {
+            get
+            {
+                return this.tenorNameFieldSpecified;
+            }
+            set
+            {
+                this.tenorNameFieldSpecified = value;
+            }
+        }
+    }
+    public partial class moexSpfiReport
+    {
+        [System.Xml.Serialization.XmlAttributeAttribute(Form = System.Xml.Schema.XmlSchemaForm.Qualified, Namespace = "http://www.w3.org/2001/XMLSchema-instance")]
+        public string schemaLocation{get;set;}
+    }
+}
