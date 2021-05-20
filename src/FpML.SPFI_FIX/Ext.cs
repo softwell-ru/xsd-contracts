db083b87bf2aae2cf68a3ec5f73151f535545b77
lppt <tvorimir@gmail.com>
2021-05-20 15:26:35 +0300

    добавил игнор для поля дата в CSA

--- src/FpML.SPFI_FIX/Ext.cs
+++ src/FpML.SPFI_FIX/Ext.cs
@@ -3,21 +3,39 @@ using System.Xml.Serialization;
 
 namespace FpML.SPFI_FIX
 {
-    public partial class FxSingleLeg
+  public partial class FxSingleLeg
+  {
+    private bool tenorNameFieldSpecified;
+    /// <remarks/>
+    [System.Xml.Serialization.XmlIgnoreAttribute()]
+    public bool tenorNameSpecified
     {
-        private bool tenorNameFieldSpecified;
-        /// <remarks/>
-        [System.Xml.Serialization.XmlIgnoreAttribute()]
-        public bool tenorNameSpecified
-        {
-            get
-            {
-                return this.tenorNameFieldSpecified;
-            }
-            set
-            {
-                this.tenorNameFieldSpecified = value;
-            }
-        }
+      get
+      {
+        return this.tenorNameFieldSpecified;
+      }
+      set
+      {
+        this.tenorNameFieldSpecified = value;
+      }
     }
+  }
+
+  public partial class CreditSupportAgreement
+  {
+    private bool dateFieldSpecified;
+
+    [System.Xml.Serialization.XmlIgnoreAttribute()]
+    public bool dateSpecified
+    {
+      get
+      {
+        return this.dateFieldSpecified;
+      }
+      set
+      {
+        this.dateFieldSpecified = value;
+      }
+    }
+  }
 }
